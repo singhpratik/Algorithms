c5b0a21856937e18581fc3c52322f9fce922e06c
Pratik Singh <pratik@Pratiks-MacBook-Pro.local>
2017-07-14 17:44:03 -0400

    initial commit

---
+++ BinaryTree/src/binarytree/Node.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarytree;
+
+/**
+ *
+ * @author pratik
+ */
+public class Node {
+    
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(){
+    
+    }
+    
+    public Node(int data){
+    
+        this.data = data;
+        left=null;
+        right = null;
+        
+    }
+    
+}
